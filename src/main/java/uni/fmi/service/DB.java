e161ccc2287ae00ec3a8c809774a066b69911d89
Rafet Halil <65808671+rafchoh@users.noreply.github.com>
2022-02-09 08:46:04 +0200

    Update DB.java

--- src/main/java/uni/fmi/service/DB.java
+++ src/main/java/uni/fmi/service/DB.java
@@ -8,12 +8,14 @@ import uni.fmi.models.User;
 
 public class DB {
 
-	// userTypes: 0-admin / 1-normal user / 2-kid 
-		public static List<User> getUser() {
-		List<User> result = new ArrayList<User>();
+	/**
+	 * userTypes: 0-admin / 1-normal user / 2-kid 
+	 */
+	public static List<User> getUser() {
+	    List<User> result = new ArrayList<User>();
 	    User u1 = new User();
 	    
-		u1.setUserType("0");
+	    u1.setUserType("0");
 	    u1.setUsername("ivanella11");
 	    u1.setPassword("123456");
 	    
@@ -25,10 +27,10 @@ public class DB {
 	    return result;
 	}
 	public static List<User> getUsers() {
-		List<User> result = new ArrayList<User>();
+	    List<User> result = new ArrayList<User>();
 	    User u1 = new User();
 	    
-		u1.setUserType("0");
+	    u1.setUserType("0");
 	    u1.setUsername("ivanella11");
 	    u1.setPassword("123456");
 	    u1.setEmail("iv11_v@abv.bg");	    
@@ -43,41 +45,41 @@ public class DB {
 	}
 	
 	public static List<Child> getChildren() {	
-        List<Child> result = new ArrayList<Child>();
-        Child c1 = new Child();
-        Child c2 = new Child();
-        Child c3 = new Child();
+            List<Child> result = new ArrayList<Child>();
+            Child c1 = new Child();
+            Child c2 = new Child();
+            Child c3 = new Child();
 		
-        c1.setFullName("Иван Петров");
-        c1.setWorkingMother("1");
-        c1.setWorkingFather("1");
-        c1.setDisability("0");
-        c1.setTwin("0");
-        c1.setRelative("1");
-        c1.setTotalPoints("3");
+            c1.setFullName("Иван Петров");
+            c1.setWorkingMother("1");
+            c1.setWorkingFather("1");
+            c1.setDisability("0");
+            c1.setTwin("0");
+            c1.setRelative("1");
+            c1.setTotalPoints("3");
         
-        c2.setFullName("Иванa Петровa");
-        c2.setWorkingMother("1");
-        c2.setWorkingFather("1");
-        c2.setDisability("0");
-        c2.setTwin("0");
-        c2.setRelative("0");
-        c2.setTotalPoints("2");
+            c2.setFullName("Иванa Петровa");
+            c2.setWorkingMother("1");
+            c2.setWorkingFather("1");
+            c2.setDisability("0");
+            c2.setTwin("0");
+            c2.setRelative("0");
+            c2.setTotalPoints("2");
         
-        c3.setFullName("Георги Атанасов");
-        c3.setWorkingMother("1");
-        c3.setWorkingFather("1");
-        c3.setDisability("0");
-        c3.setTwin("0");
-        c3.setRelative("0");
-        c3.setTotalPoints("2");
+            c3.setFullName("Георги Атанасов");
+            c3.setWorkingMother("1");
+            c3.setWorkingFather("1");
+            c3.setDisability("0");
+            c3.setTwin("0");
+            c3.setRelative("0");
+            c3.setTotalPoints("2");
         
         
-        result.clear();
+            result.clear();
         
-        result.add(c1);
-        result.add(c2);
-        result.add(c3);
+            result.add(c1);
+            result.add(c2);
+            result.add(c3);
 	    
         return result;
     }
